d7cd412bb73da57df6b8c238b690dc007f10d3c2
YimLingLoo <68176566+YimLingLoo@users.noreply.github.com>
2023-09-25 16:58:54 +0800

    Merge components branch (#1) to main
    * Added 3 components
    
    Added header, selected items and post
    
    * Update Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+function Header() {
+    return (
+        <header className='header-component'>
+            <h1>YL Ottergram Application</h1>
+        </header>
+    )
+}
+
+export default Header;
